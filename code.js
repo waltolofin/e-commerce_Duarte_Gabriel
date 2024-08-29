75f9483c342a83f67526bd57b3ba1f70883e16df
Waltolofin <nombre@gmail.com>
2024-08-29 20:28:24 -0300

    Modifique elementos desde js

--- code.js
+++ code.js
@@ -1,1 +1,7 @@
-console.log("HOLA");
+let img = document.querySelector(".card-img-top");
+let h5 = document.querySelector(".card-title");
+let parrafo = document.querySelector(".card-text");
+
+img.src = "https://i.redd.it/wxb33zg5lk561.jpg";
+h5.textContent = "GOKU";
+parrafo.textContent = "Soy un ser proveniente del planeta vegeta Fui criado en la tierra obteniendo fuerza infinita Con mi ki te mandaré volando como una dinamita Has encontrado un enemigo peor que la criptonita";
