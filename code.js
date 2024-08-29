54af1ba813144e0f14071a08f347c039f55e6fa6
Waltolofin <nombre@gmail.com>
2024-08-29 20:49:14 -0300

    Modifique las 3 cards

--- code.js
+++ code.js
@@ -1,7 +1,17 @@
-let img = document.querySelector(".card-img-top");
-let h5 = document.querySelector(".card-title");
-let parrafo = document.querySelector(".card-text");
+let img1 = document.querySelectorAll(".card-img-top");
+let h51 = document.querySelectorAll(".card-title");
+let parrafo1 = document.querySelectorAll(".card-text");
 
-img.src = "https://i.redd.it/wxb33zg5lk561.jpg";
-h5.textContent = "GOKU";
-parrafo.textContent = "Soy un ser proveniente del planeta vegeta Fui criado en la tierra obteniendo fuerza infinita Con mi ki te mandaré volando como una dinamita Has encontrado un enemigo peor que la criptonita";
+img1[0].src = "https://i.redd.it/wxb33zg5lk561.jpg";
+h51[0].textContent = "GOKU";
+parrafo1[0].textContent = "Soy un ser proveniente del planeta vegeta Fui criado en la tierra obteniendo fuerza infinita Con mi ki te mandaré volando como una dinamita Has encontrado un enemigo peor que la criptonita";
+
+img1[1].src = "https://cdn.elcocinerocasero.com/imagen/receta/1200/2024-02-01-20-03-28/milanesa-de-ternera-a-la-napolitana.jpeg";
+h51[1].textContent = "MILANESA";
+parrafo1[1].textContent = "La milanesa es un filete, normalmente de carne vacuna, empanado, el cual se cocina frito o al horno";
+
+img1[2].src = "https://m.media-amazon.com/images/I/81i+hA9UVdL._AC_SL1500_.jpg";
+h51[2].textContent = "TINKIWINKI";
+parrafo1[2].textContent = "Dios omnipotente creador de el cielo y la tierra";
+
+
