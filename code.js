72a150638c571e6aad46e80c5aa3e72c9f7d0d12
Waltolofin <nombre@gmail.com>
2024-09-17 18:49:19 -0300

    meti todo dentro de una funcion

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
-let h1 = document.querySelector("h1");
+
+function cards() {
+  let h1 = document.querySelector("h1");
 h1.innerHTML = "Productos";
 let contenedor = document.querySelector(".container");
 
@@ -11,13 +13,17 @@ for (let i = 1; i <= 9; i++) {
   <div class="card-body">
     <h5 class="card-title">Card ${i}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-primary">Ver más</a>
   </div>
 </div>
 `);
 }
 
 contenedor.innerHTML = arr.join(``);
+
+}
+
+cards();
 
 
 
