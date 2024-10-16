ffada4f09758f79d352bd5644876f0a7aaf605ce
Waltolofin <nombre@gmail.com>
2024-10-15 22:02:40 -0300

    nose

--- code.js
+++ code.js
@@ -2,7 +2,9 @@
   let h1 = document.querySelector("h1");
   h1.innerHTML = "Productos";
   let contenedor = document.querySelector(".container");
-  let card = data.map((car)=>`<div class="card" id="${car.id}" >
+//Usamos cada uno de los objetos de data para armar cards en el html
+function mapear(objmapear) {
+    let card = objmapear.map((car)=>`<div class="card" id="${car.id}" >
   <img src="${car.img}" class="card-img-top" alt="${car.title}">
   <div class="card-body">
     <h5 class="card-title">${car.title}</h5>
@@ -10,64 +12,48 @@
     <a href="producto.html?prod=${car.id}" class="btn btn-primary">Ver más</a>
   </div>
 </div>`);
-
 contenedor.innerHTML = card.join('');
+  }
+mapear(data);
 
-
+//Seleccionamos los botones y imputs necesarios
 let filterButton = document.getElementById("filter-button");
 let input = document.getElementById("input-value");
 let resetButton = document.getElementById("clear-button");
 
+///filtramos segun lo ingresado en el imput
 const filtrar = (event) =>{
   event.preventDefault();
   let filtered = data.filter((item) => item.title.toLowerCase().includes(input.value.toLowerCase()));
-  card = filtered.map((filter) => `<div class="card" id="${filter.id}" >
-   <img src="${filter.img}" class="card-img-top" alt="Auto">
-   <div class="card-body">
-     <h5 class="card-title">Card ${filter.title}</h5>
-     <p class="card-text">${filter.detail}</p>
-     <a href="producto.html?prod=${filter.id}" class="btn btn-primary">Ver más</a>
-   </div>
- </div>`)
+  if (filtered.length != 0) {
+    mapear(filtered);
+  } else{
+    contenedor.innerHTML = `<div class = "p-noencontrado"><p class="fw-bold fs-1">ERROR<p/><span class="material-symbols-outlined">
+error</span><p class="fw-bold">"Parece que no tenemos lo que buscas. ¿Te gustaría intentar con otras palabras o explorar nuestras categorías populares?"</p></div>`;
+  }
   
-contenedor.innerHTML = card.join('');
 }
 
+///reseteamos lo buscado y agregamos todas las cards de nuevo
 const reset = (event) =>{
   event.preventDefault();
-  card = data.map((car)=>`<div class="card" id="${car.id}" >
-  <img src="${car.img}" class="card-img-top" alt="${car.title}">
-  <div class="card-body">
-    <h5 class="card-title">${car.title}</h5>
-    <p class="card-text">${car.detail}</p>
-    <a href="producto.html?prod=${car.id}" class="btn btn-primary">Ver más</a>
-  </div>
-</div>`);
-contenedor.innerHTML = card.join('');
-input.value = "";
+  mapear(data);
+  input.value = "";
 }
 
 filterButton.addEventListener("click", filtrar);
 resetButton.addEventListener("click",reset);
 
+///seleccionamos las categorias y las filtramos para mostrar solo por categoria y despues le añadimos con un for los eventos
 let arrCategoria = document.querySelectorAll(".category-filter");
 
 const verSneakers= (cate) =>{
   let filtered = data.filter((item) => item.category.toLowerCase().includes(cate));
-  card = filtered.map((filter) => `<div class="card" id="${filter.id}" >
-   <img src="${filter.img}" class="card-img-top" alt="Auto">
-   <div class="card-body">
-     <h5 class="card-title">Card ${filter.title}</h5>
-     <p class="card-text">${filter.detail}</p>
-     <a href="producto.html?prod=${filter.id}" class="btn btn-primary">Ver más</a>
-   </div>
- </div>`)
-  
-contenedor.innerHTML = card.join('');
+  mapear(filtered)
+}
+for (const i of arrCategoria) {
+  i.addEventListener("click", () => verSneakers(i.textContent.toLowerCase()));
 } 
-arrCategoria[0].addEventListener("click", () => verSneakers(arrCategoria[0].textContent.toLowerCase()));
-arrCategoria[1].addEventListener("click", () => verSneakers(arrCategoria[1].textContent.toLowerCase()));
-arrCategoria[2].addEventListener("click", () => verSneakers(arrCategoria[2].textContent.toLowerCase()));
 
 
 
